772a330c2f0ce8f502942b0f79982c100c25369d
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-11-09 17:48:57 +0100

    initialisation de la liste des salles

---
+++ src/components/ListCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import CardRoom from '@/components/CardRoom.vue'
+
+defineProps<{
+  listRoom: Array<T>
+}>()
+</script>
+
+<template>
+  <ul class="flex flex-col gap-4 w-fit m-auto">
+    <li v-for="(room, index) in listRoom" :key="index">
+      <CardRoom :room="room" />
+    </li>
+  </ul>
+</template>
