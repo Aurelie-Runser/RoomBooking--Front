3157038bd6671f1d943b2e9732b3f2130483c07f
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-11-13 19:08:28 +0100

    renommage composant en français

--- src/components/ListCard.vue
+++ src/components/ListCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import CardRoom from '@/components/CardRoom.vue'
+import CardSalle from '@/components/CardSalle.vue'
 
 defineProps<{
   listRoom: Array<T>
@@ -9,7 +9,7 @@ defineProps<{
 <template>
   <ul class="flex flex-col gap-4 w-fit m-auto p-2">
     <li v-for="(room, index) in listRoom" :key="index">
-      <CardRoom :room="room" />
+      <CardSalle :room="room" />
     </li>
   </ul>
 </template>
