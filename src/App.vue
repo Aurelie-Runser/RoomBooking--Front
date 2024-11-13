49d7c10a8deeb435a9b8ceee3599505e8fbd5019
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-11-13 19:08:04 +0100

    création du menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import MenuComp from './components/MenuComp.vue'
 </script>
 
 <template>
+  <MenuComp />
   <RouterView />
   <!-- Contenu des pages -->
 </template>
