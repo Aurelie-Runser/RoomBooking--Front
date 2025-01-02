c2b46a93324a3a3aa8de500432e0e3ced7ab889f
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2025-01-02 17:28:15 +0100

    ajout de la fonction GetRoomById

--- src/domain/services/roomService.ts
+++ src/domain/services/roomService.ts
@@ -9,3 +9,13 @@ export async function GetRooms() {
     throw error
   }
 }
+
+export async function GetRoomById(id: number) {
+  try {
+    const response = await apiClient.get('/room/' + id)
+    return response.data
+  } catch (error) {
+    console.error('Erreur lors de la récupération de la salle :', error)
+    throw error
+  }
+}
