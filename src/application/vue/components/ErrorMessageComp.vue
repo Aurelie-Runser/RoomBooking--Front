565a3265807c4036a020b871c0a8b19151af43a1
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2025-01-12 14:23:58 +0100

    suppression bouton accueil dans ErrorMessageComp

--- src/application/vue/components/ErrorMessageComp.vue
+++ src/application/vue/components/ErrorMessageComp.vue
@@ -1,13 +1,5 @@
 <template>
-  <div>
-    <p class="text-center m-6 p-4 bg-red-100 border border-red-500">
-      <slot /> &#x1F61F;
-    </p>
-
-    <div class="flex justify-center gap-6">
-      <RouterLink to="/" class="p-4 bg-blue-200 hover:bg-blue-300 rounded-md">
-        Accueil
-      </RouterLink>
-    </div>
-  </div>
+  <p class="text-center m-6 p-4 bg-red-100 border border-red-500">
+    <slot /> &#x1F61F;
+  </p>
 </template>
