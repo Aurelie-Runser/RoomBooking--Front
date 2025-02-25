300b6ab11bdf92d20f88abc5ac571821355f4ff3
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2025-02-25 23:12:11 +0100

    suppression des packages inutiles

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import './application/vue/assets/index.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './application/vue/router'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
